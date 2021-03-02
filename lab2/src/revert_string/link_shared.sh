fd00c463a3bc8a961034e2a14b110829549d32fd
repl.it user <replituser@example.com>
2021-03-02 07:27:10 +0000

    reverse_string vs revert_string

--- lab2/src/revert_string/link_shared.sh
+++ lab2/src/revert_string/link_shared.sh
@@ -1,2 +1,2 @@
 gcc -c main.c -o obj/main.o
-gcc obj/main.o -Lshared -lrevert_string -o shared/reverse_string
+gcc obj/main.o -Lshared -lrevert_string -o shared/revert_string
--- lab2/src/revert_string/link_static.sh
+++ lab2/src/revert_string/link_static.sh
@@ -1,2 +1,2 @@
 gcc -c main.c -o obj/main.o
-gcc obj/main.o -Lstatic -lrevert_string -o static/reverse_string
+gcc obj/main.o -Lstatic -lrevert_string -o static/revert_string
